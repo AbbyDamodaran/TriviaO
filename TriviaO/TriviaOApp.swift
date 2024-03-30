57810696df440a8991876bd7b39ceae3c26b33ee
Abby Damodaran <abbydamodaran@Abbys-MacBook-Air.local>
2024-03-29 22:04:51 -0700

    Initial code

---
+++ TriviaO/TriviaOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaOApp.swift
+//  TriviaO
+//
+//  Created by Abby Damodaran on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
